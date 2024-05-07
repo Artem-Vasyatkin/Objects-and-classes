7e6a8f819d8f11d7dd7bbb670cd7690906b98faf
Artem Vasyatkin <dustler98@gmail.com>
2024-05-07 23:37:07 +0300

    Homework done

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,30 @@
 public class Book {
-    String name;
-    int age;
-    Author author;
+    private String name;
+    private int yearOfPublicationOfTheBook;
+    private Author author;
 
+    public Book(String name, int yearOfPublicationOfTheBook, Author author) {
+        this.name = name;
+        this.yearOfPublicationOfTheBook = yearOfPublicationOfTheBook;
+        this.author = author;
+    }
 
+    public void setYearOfPublicationOfTheBook(int year) {
+        yearOfPublicationOfTheBook = year;
     }
+
+    public String getName() {
+        return name;
     }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYearOfPublicationOfTheBook() {
+        return yearOfPublicationOfTheBook;
+    }
+
+}
+
 
--- src/Main.java
+++ src/Main.java
@@ -27,9 +27,25 @@ public class Main {
 
         System.out.println("Homework");
 
+        Author sagan = new Author("Carl", "Sagan");
+        Author deutsch = new Author("David", "Deutsch");
 
+        Book book1 = new Book("A world full of demons",
+                2022, sagan);
+        Book book2 = new Book("The beginning of infinity",
+                2021, deutsch);
+        book2.setYearOfPublicationOfTheBook(2011);
+
+        System.out.println("Название книги - " + book1.getName() + "," +
+                " Год выпуска - " + book1.getYearOfPublicationOfTheBook() + "," +
+                " Автор книги - " + sagan.getName() + " " + sagan.getSurname());
+
+        System.out.println("Название - " + book2.getName() + "," +
+                " Год выпуска - " + book2.getYearOfPublicationOfTheBook() + "," +
+                " Автор книги - " + deutsch.getName() + " " + deutsch.getSurname());
 
     }
+
 
 
 }
